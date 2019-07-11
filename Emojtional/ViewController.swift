801d2813e2dca0e39e26835feb0c98dcac71139d
mayasykes1 <sykesmaya@gmail.com>
2019-07-11 16:31:14 -0700

    Initial Commit

---
+++ Emojtional/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Emojtional
+//
+//  Created by Apple on 7/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heartFaceButton(_ sender: Any) {
+    }
+    @IBAction func showMessage(sender: UIButton) {
+        
+    }
+
+    
+    
+    @IBAction func oyFaceButton(_ sender: Any) {
+    }
+    @IBAction func showMessage(sender: UIButton) {
+    }
+
