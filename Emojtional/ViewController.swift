cb3c8dad93d30ffa7e4c12fd62917042d01c58d0
mayasykes1 <sykesmaya@gmail.com>
2019-07-12 11:14:30 -0700

    Initial Commit

--- Emojtional/ViewController.swift
+++ Emojtional/ViewController.swift
@@ -10,21 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     let emojis = ["🥰": "bae face", "🥴": "swag face"]
+    
+    let customMessages = ["bae face": ["ur swag", "nice chakras", "my dog would like u"], "swag face": ["step on a lego", "u have papaya feet"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func heartFaceButton(_ sender: Any) {
-    }
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+
+        //I want a variety of messages pop up when the user clicks on each emojji
         
-    }
+        let alertController = UIAlertController(title: selectedEmotion, message: customMessages[emojis[selectedEmotion!], preferredStyle:UIAlertController.Style.alert)
+        
+        
+        
+         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
 
     
+   
     
-    @IBAction func oyFaceButton(_ sender: Any) {
-    }
-    @IBAction func showMessage(sender: UIButton) {
-    }
+
+ 
+
+    
+
+}
+
 
